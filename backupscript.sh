805cc3a98529cf33640afa6a29da884d83e1a903
izanamisu <78270523+izanamisu@users.noreply.github.com>
2022-02-19 17:41:00 +0100

    before script

--- backupscript.sh
+++ backupscript.sh
@@ -1,9 +1,10 @@
-sudo rsync -avzh /home/pi/klipper /tmp/backups/
-sudo rsync -avzh /home/pi/klipper_config /tmp/backups/
-sudo rsync -avzh /home/pi/klipper_logs /tmp/backups/
-sudo rsync -avzh /home/pi/klippy-env /tmp/backups/
-sudo rsync -avzh /home/pi/fluidd /tmp/backups/
-sudo rsync -avzh /home/pi/gcode_files /tmp/backups/
-sudo rsync -avzh /home/pi/moonraker /tmp/backups/
-sudo rsync -avzh /home/pi/moonraker-env /tmp/backups/
-sudo rsync -avzh /boot /tmp/backups/
+#Making backup before setup
+sudo rsync -avzh /home/pi/klipper /tmp/backups/before
+sudo rsync -avzh /home/pi/klipper_config /tmp/backups/before
+sudo rsync -avzh /home/pi/klipper_logs /tmp/backups/before
+sudo rsync -avzh /home/pi/klippy-env /tmp/backups/before
+sudo rsync -avzh /home/pi/fluidd /tmp/backups/before
+sudo rsync -avzh /home/pi/gcode_files /tmp/backups/before
+sudo rsync -avzh /home/pi/moonraker /tmp/backups/before
+sudo rsync -avzh /home/pi/moonraker-env /tmp/backups/before
+sudo rsync -avzh /boot /tmp/backups/before
