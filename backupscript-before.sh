a7747619fa5b1c6b2438b1f380c5b8e0dd467b83
izanamisu <78270523+izanamisu@users.noreply.github.com>
2022-02-19 17:44:01 +0100

    before

---
+++ backupscript-before.sh
@@ -0,0 +1,10 @@
+#Making backup before setup
+sudo rsync -avzh /home/pi/klipper /tmp/backups/before
+sudo rsync -avzh /home/pi/klipper_config /tmp/backups/before
+sudo rsync -avzh /home/pi/klipper_logs /tmp/backups/before
+sudo rsync -avzh /home/pi/klippy-env /tmp/backups/before
+sudo rsync -avzh /home/pi/fluidd /tmp/backups/before
+sudo rsync -avzh /home/pi/gcode_files /tmp/backups/before
+sudo rsync -avzh /home/pi/moonraker /tmp/backups/before
+sudo rsync -avzh /home/pi/moonraker-env /tmp/backups/before
+sudo rsync -avzh /boot /tmp/backups/before
