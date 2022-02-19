4001724e4d29bec557f823b11902787cd89fefd2
izanamisu <78270523+izanamisu@users.noreply.github.com>
2022-02-19 17:33:32 +0100

    rsyncbackupscript

---
+++ backupscript.sh
@@ -0,0 +1,9 @@
+sudo rsync -avzh /home/pi/klipper /tmp/backups/
+sudo rsync -avzh /home/pi/klipper_config /tmp/backups/
+sudo rsync -avzh /home/pi/klipper_logs /tmp/backups/
+sudo rsync -avzh /home/pi/klippy-env /tmp/backups/
+sudo rsync -avzh /home/pi/fluidd /tmp/backups/
+sudo rsync -avzh /home/pi/gcode_files /tmp/backups/
+sudo rsync -avzh /home/pi/moonraker /tmp/backups/
+sudo rsync -avzh /home/pi/moonraker-env /tmp/backups/
+sudo rsync -avzh /boot /tmp/backups/
